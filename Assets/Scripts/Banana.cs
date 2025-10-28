344cfc5c59a207c21d0b28c6a17bc51a42181f9b
RivadonS <rivadon.sung@bumail.net>
2025-10-28 15:22:48 +0700

    Update Player, Create Banana Code

---
+++ Assets/Scripts/Banana.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Banana : Weapon
+{
+    public override void Move()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnHitWith(Character character)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,42 @@
 using UnityEngine;
 
-public class Player : Character
+public class Player : Character, IShootable
 {
+    [field: SerializeField] public GameObject Bullet { get; set; }
+    [field: SerializeField] public Transform ShootPoint { get; set; }
+    [field: SerializeField] public float ReloadTime { get; set;}
+    [field: SerializeField] public float WaitTime { get; set;}
+
     void Start()
     {
         base.Initialize(100); //set health for player
+        ReloadTime = 1.0f;
+        WaitTime = 0.0f;
+    }
+
+    void FixedUpdate()
+    {
+        WaitTime += Time.fixedDeltaTime;
     }
 
     void Update()
     {
+        Shoot();
+    }
 
+    public void Shoot()
+    {
+        if ( Input.GetButtonDown("Fire1") && WaitTime >= ReloadTime)
+        {
+            var bullet = Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
+            Banana banana = bullet.GetComponent<Banana>();
+            if (banana != null)
+            {
+                banana.InitWeapon(20, this);
+            }
+
+            WaitTime = 0.0f;
+        }
     }
 
     public void OnHitWith(Enemy enemy)
@@ -17,11 +44,12 @@ public class Player : Character
         TakeDamage(enemy.DamageHit);
     }
 
-    public void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         Enemy enemy = collision.gameObject.GetComponent<Enemy>();
         if (enemy != null) {
             OnHitWith(enemy);
+            Debug.Log($"{this.name} collides with {enemy.name}!");
         }
     }
 }
